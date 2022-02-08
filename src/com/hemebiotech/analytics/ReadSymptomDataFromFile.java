259e551256cedd0e1520f1d179742a431be0387b
Jérôme Pagny <pagny.jerome@gmail.com>
2022-02-08 12:11:37 +0100

    Fix SonarLint alerts for ReadSymptomDataFromFile + ISymptomReader

--- src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
+	private final String filepath;
 	
 	/**
 	 * 
@@ -23,19 +23,21 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	}
 	
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
+	public List<String> getSymptoms() {
+
+		ArrayList<String> result = new ArrayList<>();
 		
 		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+
+			try (BufferedReader reader = new BufferedReader (new FileReader(filepath))){
+
 				String line = reader.readLine();
 				
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
 				}
-				reader.close();
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
